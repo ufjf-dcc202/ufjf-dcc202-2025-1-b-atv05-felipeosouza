b0b8adb0ca6fc9fc350e22a263a6a648fee8d3ab
Felipe Souza <souza.felipe@estudante.ufjf.br>
2025-07-30 16:36:00 -0300

    adiciona tabuleiro

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const eH1 = document.querySelector('h1');
 eH1.textContent = "Olá mundo!";
+
+const eTabuleiro = criaTabuleiro();
+document.body.append();
+
+function criaTabuleiro(){
+    const novoTabuleiro = document.createElement('div');
+    novoTabuleiro.classList.add("tabuleiro") ;
+    return novoTabuleiro;
+}
