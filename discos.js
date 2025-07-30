c0f077dd228e435b9bb4a399663e78d9459a2508
Felipe Souza <souza.felipe@estudante.ufjf.br>
2025-07-30 17:20:59 -0300

    adiciona estado na tela

---
+++ discos.js
@@ -0,0 +1,18 @@
+const tabuleiro = ["preto", "preto", "preto", "transparente", "branco", "branco", "branco"];
+
+export {getTabuleiro};
+
+function getTabuleiro(){
+    return [...tabuleiro];
+}
+
+function mover(origem, destino){
+    if(tabuleiro[destino] !== "transparente") return;
+    if(Math.abs(destino - origem) > 2) return;
+
+    if(destino-origem === 2 && tabuleiro[origem+1]!== "transparente") return;
+    if(destino-origem === -2 && tabuleiro[origem-1]!== "transparente")return;
+
+    tabuleiro[destino] = tabuleiro[origem];
+    tabuleiro[origem] = "transparente";
+}
--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+import { getTabuleiro } from "./discos.js";
+
 const eH1 = document.querySelector('h1');
 eH1.textContent = "Olá mundo!";
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+const tabuleiro = getTabuleiro();
+for (let i = 0; i < 7; i++) {
+    const eDisco = criaDisco(tabuleiro[i]);
+    eTabuleiro.append(eDisco);
+}
+
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement('div');
     novoTabuleiro.classList.add("tabuleiro");
+
     return novoTabuleiro;
+}
+
+function criaDisco(cor){
+    const novoDisco = document.createElement('div');
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+
+    return novoDisco;
 }
