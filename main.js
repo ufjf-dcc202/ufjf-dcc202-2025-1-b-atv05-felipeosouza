ba9dfe193104510a3593eba489412b033e2ac379
Felipe Souza <souza.felipe@estudante.ufjf.br>
2025-07-30 16:40:40 -0300

    cria estilo tabuleiro

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const eH1 = document.querySelector('h1');
 eH1.textContent = "Olá mundo!";
 
 const eTabuleiro = criaTabuleiro();
-document.body.append();
+document.body.append(eTabuleiro);
 
 function criaTabuleiro(){
     const novoTabuleiro = document.createElement('div');
-    novoTabuleiro.classList.add("tabuleiro") ;
+    novoTabuleiro.classList.add("tabuleiro");
     return novoTabuleiro;
 }
